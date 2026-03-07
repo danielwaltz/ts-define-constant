89f2dd5fd13268c89974e97403d43384a0f569ab
Daniel Waltz <danielbwaltz@gmail.com>
2026-03-07 07:23:59 -0500

    perf: move regex to module scope

--- src/utils/pascalCase.ts
+++ src/utils/pascalCase.ts
@@ -1,11 +1,15 @@
+const IS_SINGLE_WORD_UNICODE_ALNUM_RE = /^[\p{L}\d]+$/iu;
+const UNICODE_ALNUM_WORD_PARTS_RE = /([\p{L}\d])([\p{L}\d]*)/giu;
+const NON_UNICODE_ALNUM_RE = /[^\p{L}\d]/giu;
+
 export function pascalCase(str: string) {
-  if (/^[\p{L}\d]+$/iu.test(str)) {
+  if (IS_SINGLE_WORD_UNICODE_ALNUM_RE.test(str)) {
     return str.charAt(0).toUpperCase() + str.slice(1);
   }
   return str
     .replaceAll(
-      /([\p{L}\d])([\p{L}\d]*)/giu,
+      UNICODE_ALNUM_WORD_PARTS_RE,
       (_g0, g1, g2) => g1.toUpperCase() + g2.toLowerCase(),
     )
-    .replaceAll(/[^\p{L}\d]/giu, "");
+    .replaceAll(NON_UNICODE_ALNUM_RE, "");
 }
